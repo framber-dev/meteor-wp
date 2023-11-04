2d7a63d08706f36ea13be1bf14e7d91807d363ac
Framber dev <138418943+framber-dev@users.noreply.github.com>
2023-11-04 17:00:21 -0400

    Create search.php

---
+++ search.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+		
+<!-- {{ Busqueda }} --> 	
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+
+<?php endwhile; else : ?>
+<p>Lo siento, no hemos encontrado ningún post.</p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
